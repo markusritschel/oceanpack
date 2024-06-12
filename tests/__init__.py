54511db81a4dc3972d0cc63300c7555505a512c3
markusritschel <git@markusritschel.de>
2024-06-12 19:09:59 +0200

    Remove dummy code

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding utf-8 -*-
-#
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Author: Markus Ritschel
-# eMail:  git@markusritschel.de
-# Date:   2024-06-12
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-#
