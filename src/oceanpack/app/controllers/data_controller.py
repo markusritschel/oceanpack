3fa18a3ef252e82a4939b45a5e8aed24b6f0909b
markusritschel <git@markusritschel.de>
2024-06-13 14:23:17 +0200

    Add data controller

---
+++ src/oceanpack/app/controllers/data_controller.py
@@ -0,0 +1,26 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-06-13
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+from ..views.data_view import DataView
+from ..models.filesource import FileSourceModel
+
+
+class DataController:
+    def __init__(self, source_type: str = None):
+        self.model = FileSourceModel(source_type)
+        self.view = DataView()
+
+    def load_data(self, path: str):
+        self.model.load_data(path)
+        self.model.clean_data()
+        self.model.process_data()
+
+    def display(self):
+        self.view.display(self.model)
+
+    def write_output(self, path):
+        data = self.model.get_data()
+        self.view.export_to_netcdf(data, path)
