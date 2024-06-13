d7381a3d4887cb529676f8929aed6f9c4e4a8f62
markusritschel <git@markusritschel.de>
2024-06-13 15:57:36 +0200

    Add method to InternalFileHandler to read file and metadata
    Metadata are retrieved by parsing the file header

--- src/oceanpack/app/models/filehandler.py
+++ src/oceanpack/app/models/filehandler.py
@@ -4,7 +4,12 @@
 # Date:   2024-06-13
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
+import logging
 from abc import ABC, abstractmethod
+
+import pandas as pd
+
+log = logging.getLogger(__name__)
 
 
 class FileHandlerInterface(ABC):
@@ -18,7 +23,79 @@ class FileHandlerInterface(ABC):
 
 
 class InternalFileHandler(FileHandlerInterface):
-    """File handler for log files created by OceanPack Analyzer or NetDI unit."""
+    """File handler for log files created by OceanPack Analyzer or NetDI unit.
+
+    Methods
+    -------
+    read_file(file_path)
+        Reads the log file and returns a data and metadata as :class:`pandas.DataFrame`, respectively.
+    """
+    @staticmethod
+    def read_file(file_path) -> pd.DataFrame:
+        """
+        Reads the log file and returns a data and metadata as :class:`pandas.DataFrame`, respectively.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to the log file.
+
+        Returns
+        -------
+        df : pandas DataFrame
+            The data from the log file as a DataFrame.
+        meta : pandas DataFrame
+            The metadata associated with the log file.
+        """
+        header = InternalFileHandler.parse_header(file_path)
+        if not header:
+            return pd.DataFrame(), pd.DataFrame()
+        
+        names = header['names']
+        units = header['units']
+        sensors = header['sensors']
+
+        data = pd.read_csv(file_path, sep=',', skiprows=header['nrows'], names=names,
+                         encoding='iso-8859-1',
+                         usecols=range(len(names)),
+                        )
+        data = data.where(data['@NAME']=='@DATA')
+        data.index = pd.to_datetime(data['DATE'] + ' ' + data['TIME'])
+        data.index.name = 'time'
+        data.drop(['@NAME', 'DATE', 'TIME', 'DATE_TIME'], axis=1, inplace=True, errors='ignore')
+
+        metadata = pd.DataFrame.from_records(list(zip(names, units, sensors))[1:], 
+                                             columns=['name', 'unit', 'device'])
+
+        return data, metadata
+
+
+    @staticmethod
+    def parse_header(file_path):
+        header_dict = {}
+        with open(file_path, 'r', encoding='Windows 1252') as f:
+            header_dict['nrows'] = 0
+            while True:
+                line = f.readline()
+                header_dict['nrows'] += 1
+                if line.startswith('@RATE') : 
+                    break
+
+                elif line.startswith('@NAME'):
+                    names = line.strip().split(',')
+                    header_dict['names'] = [x.replace('/', '_') for x in names]
+
+                elif line.startswith('@UNIT'):
+                    header_dict['units'] = line.strip().split(',')
+
+                elif line.startswith('@SENSOR'):
+                    header_dict['sensors'] = line.strip().split(',')
+
+                if header_dict['nrows'] > 15:
+                    log.warning(f'Could not find header in file {file_path}. Skip file.')
+                    return None
+        return header_dict
+
 
 
 class AnalyzerFileHandler(InternalFileHandler):
