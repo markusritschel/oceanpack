5e9bb272299c716374d9119c37c9ce344aca0f9b
markusritschel <git@markusritschel.de>
2024-06-13 14:31:54 +0200

    Add FileSourceType class

--- src/oceanpack/app/models/filesource.py
+++ src/oceanpack/app/models/filesource.py
@@ -4,6 +4,44 @@
 # Date:   2024-06-12
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
+from enum import Enum, IntEnum
+
+
+class FileSourceType(Enum):
+    """This class determines the file source."""
+
+    ANALYZER = 'Analyzer'
+    NETDI    = 'NetDI'
+    STREAM   = 'Stream'
+
+    @staticmethod
+    def isvalid(source_type: str) -> bool:
+        """Checks if the source type is valid."""
+        source_types = [member.value for member in FileSourceType]
+        return source_type in source_types
+    
+    def get_filehandler(self):
+        """Get the file handler for the respective source."""
+        if self == FileSourceType.ANALYZER:
+            from .filehandler import AnalyzerFileHandler
+            return AnalyzerFileHandler
+        elif self == FileSourceType.NETDI:
+            from .filehandler import NetDIFileHandler
+            return NetDIFileHandler
+        elif self == FileSourceType.STREAM:
+            from .filehandler import StreamFileHandler
+            return StreamFileHandler
+        else:
+            return None
+    
+    @classmethod
+    def from_string(cls, value: str):
+        for source in cls:
+            if source.value == value:
+                return source
+        raise ValueError(f"Invalid source type: {value}")
+
+
 class FileSourceModel:
     def __init__(self, source_type: FileSourceType = None) -> None:
         self._filehandler = None
