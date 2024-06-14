c0a39f6a3c922c7c5169378ec8c8807249910507
markusritschel <git@markusritschel.de>
2024-06-14 13:09:12 +0200

    Add data_view module

---
+++ src/oceanpack/app/views/data_view.py
@@ -0,0 +1,14 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-06-14
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+from oceanpack.app.models.filesource import FileSourceModel
+
+
+
+class DataView:
+    @staticmethod
+    def display(model: FileSourceModel):
+        pass
