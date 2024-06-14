ac3372e16fc79227db21e27b5922c6fa778e21c6
markusritschel <git@markusritschel.de>
2024-06-14 12:55:18 +0200

    Add function to compute water vapor pressure

--- src/oceanpack/utils/helpers.py
+++ src/oceanpack/utils/helpers.py
@@ -229,3 +229,21 @@ def temperature2C(T):
     return T
 
 
+def compute_water_vapor_pressure(T, S):
+    """Compute the water vapor pressure by means of the temperature [K] and the salinity [PSU]
+    following :cite:t:`weiss_nitrous_1980`.
+
+    Parameters
+    ----------
+    S: float or pd.Series
+        Salinity in PSU
+    T: float or pd.Series
+        Temperature (°C gets converted into Kelvin)
+    """
+    # °C -> K
+    T = temperature2K(T)
+
+    pH2O = np.exp(24.4543 - 67.4509*(100/T) - 4.8489*np.log(T/100) - 0.000544*S)
+    return pH2O
+
+
