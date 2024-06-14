a90a596c6fd47146c569985f05bf05987f7356e5
markusritschel <git@markusritschel.de>
2024-06-14 12:46:18 +0200

    Add utils.helpers module

---
+++ src/oceanpack/utils/helpers.py
@@ -0,0 +1,12 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-06-14
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
