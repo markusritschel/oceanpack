050854bae456d41886942200f4bfaf209003ef31
markusritschel <git@markusritschel.de>
2024-06-14 14:47:59 +0200

    Make utils a package

---
+++ src/oceanpack/utils/__init__.py
@@ -0,0 +1,11 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-06-14
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+
+
+log = logging.getLogger(__name__)
+
