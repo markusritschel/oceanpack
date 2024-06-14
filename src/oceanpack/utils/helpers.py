b6ab6fa9903a18c24cf0bb95b4c38fb7eb653f74
markusritschel <git@markusritschel.de>
2024-06-14 12:52:53 +0200

    Add function to determine the order of magnitude of some input

--- src/oceanpack/utils/helpers.py
+++ src/oceanpack/utils/helpers.py
@@ -101,3 +101,30 @@ def compute_salinity(C, T, p):
     return salinity
 
 
+def order_of_magnitude(x):
+    """Determine the order of magnitude of the numeric input (`int`, `float`, :meth:`numpy.array` or :meth:`pandas.Series`).
+
+    Examples
+    --------
+    >>> order_of_magnitude(11)
+    array(1.)
+    >>> order_of_magnitude(234)
+    array(2.)
+    >>> order_of_magnitude(1)
+    array(0.)
+    >>> order_of_magnitude(.15)
+    array(-1.)
+    >>> order_of_magnitude(np.array([24.13, 254.2]))
+    array([1., 2.])
+    >>> order_of_magnitude(pd.Series([24.13, 254.2]))
+    array([1., 2.])
+    """
+    x = np.asarray(x)
+    if np.all(x == 0):
+        return None
+    x = x[x != 0]
+    oom = np.floor(np.log10(x))
+    # oom = (np.int32(np.log10(np.abs(x))) + 1)
+    return np.array(oom)
+
+
