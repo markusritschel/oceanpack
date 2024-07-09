ab367154c4b8b6a3d307b958a3ae24eaa41f5d75
markusritschel <git@markusritschel.de>
2024-07-09 15:04:03 +0200

    Update test_filehandler.py

--- tests/test_filehandler.py
+++ tests/test_filehandler.py
@@ -2,13 +2,11 @@ import pandas as pd
 import pytest
 from oceanpack.app.models.filehandler import AnalyzerFileHandler
 
-@pytest.fixture()
-def analyzer_file():
-    return '/home/markusritschel/PycharmProjects/EUREC4A/data/raw_data/NetDI/200117_001.log'
-
 class TestAnalyzerFileHandler:
-    def test_read_file(self, analyzer_file):
+    def test_read_file(self):
+        analyzer_file = 'tests/example_op.log'
         file_handler = AnalyzerFileHandler()
         result = file_handler.read_file(analyzer_file)
         assert isinstance(result, tuple)
         assert isinstance(result[0], pd.DataFrame)
+
